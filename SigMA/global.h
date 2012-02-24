8c91899972e1da61b90419adc72f8ffd57bb291d
Dent Earl <dearl@soe.ucsc.edu>
2012-02-24 11:02:03 -0800

    fixing the line wrapping on the license information

--- SigMA/global.h
+++ SigMA/global.h
@@ -1,16 +1,32 @@
 /*VI. Conditions of Use
  *
- * Xiaoyu Chen, Amol Prakash, and Martin Tompa give permission for Dent Earl and his institution to use the StatSigmMA-w software developed at the University of Washington for research purposes, on the following conditions:
+ * Xiaoyu Chen, Amol Prakash, and Martin Tompa give permission for
+ * Dent Earl and his institution to use the StatSigmMA - w software
+ * developed at the University of Washington for research purposes, on
+ * the following conditions:
  *
- * 1. Dent Earl and his institutional colleagues may modify the StatSigmMA-w software and distribute the resulting modified software for research purposes, provided (a) that it is distributed  together with these Conditions of Use, (b) that Martin Tompa receive a copy of the finalized modified software, and (c) that Xiaoyu Chen, Amol Prakash, and Martin Tompa are credited with the authorship of the software.
- *  2. The StatSigmMA-w software will be used by you and/or your institution solely for noncommercial purposes, except with express permission from the authors.
- * 3. Any risk associated with using the StatSigmMA-w software at your institution is with you and your institution.
- * 4. StatSigmMA-w will be cited in any publication(s) reporting on data obtained from it as:
- *    Amol Prakash and Martin Tompa, "Measuring the Accuracy of Genome-Size Multiple Alignments". Genome Biology, vol. 8, issue 6, June 2007, R124.
- *    Xiaoyu Chen and Martin Tompa, "Comparative assessment of methods for aligning multiple genome sequences". Nature Biotechnology, vol. 28, no. 6, June 2010, 567-572.
-*/
-// #define DEBUG
-
+ * 1. Dent Earl and his institutional colleagues may modify the
+ *  StatSigmMA - w software and distribute the resulting modified
+ *  software for research purposes, provided (a) that it is
+ *  distributed together with these Conditions of Use, (b) that Martin
+ *  Tompa receive a copy of the finalized modified software, and (c)
+ *  that Xiaoyu Chen, Amol Prakash, and Martin Tompa are credited with
+ *  the authorship of the software.  
+ * 2. The StatSigmMA - w software
+ *  will be used by you and/or your institution solely for
+ *  noncommercial purposes, except with express permission from the
+ *  authors.  
+ * 3. Any risk associated with using the StatSigmMA - w
+ *  software at your institution is with you and your institution.
+ * 4. StatSigmMA - w will be cited in any publication(s) reporting on
+ *  data obtained from it as: 
+ * * Amol Prakash and Martin Tompa,
+ *   "Measuring the Accuracy of Genome - Size Multiple
+ *   Alignments". Genome Biology, vol. 8, issue 6, June 2007, R124.
+ * * Xiaoyu Chen and Martin Tompa, "Comparative assessment of methods
+ *   for aligning multiple genome sequences". Nature Biotechnology,
+ *   vol. 28, no. 6, June 2010, 567 - 572.  
+ */
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
 
